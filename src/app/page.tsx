b7336cc9440a80605bf6d6c099318762a3d1d443
Ayub Khan <xyzkhn0@gmail.com>
2025-07-08 18:53:03 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div
+    className="h-screen bg-cover bg-center bg-no-repeat"
+    style={{ backgroundImage: 'url("/your-image.jpg")' }}
+  >
+    {/* Your content */}
+  </div>
+  );
+}
